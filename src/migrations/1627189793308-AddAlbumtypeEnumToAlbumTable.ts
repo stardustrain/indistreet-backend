796909d6b515c53414bf7e2896af51b54e2e87a2
stardustrain <kthanterran@gmail.com>
2021-07-26 09:32:31 +0900

    Fix migration script

--- src/migrations/1627189793308-AddAlbumtypeEnumToAlbumTable.ts
+++ src/migrations/1627189793308-AddAlbumtypeEnumToAlbumTable.ts
@@ -4,8 +4,9 @@ export class AddAlbumtypeEnumToAlbumTable1627189793308 implements MigrationInter
     name = 'AddAlbumtypeEnumToAlbumTable1627189793308'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TYPE "album_albumtype_enum" AS ENUM('single', 'ep', 'miniAlbum', 'fullAlbum', 'ost')`);
+        await queryRunner.query(`CREATE TYPE "album_albumtype_enum" AS ENUM('single', 'ep', 'miniAlbum', 'fullAlbum', 'ost', 'unknown')`);
         await queryRunner.query(`ALTER TABLE "album" ADD "albumType" "album_albumtype_enum"`);
+        await queryRunner.query(`ALTER TABLE "album" ALTER COLUMN "albumType" SET DEFAULT 'unknown'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
